dcf9519c14c2860a4b137a035477cb6d0ec1a650
AB <andreabuffasbru@gmail.com>
2014-11-27 09:27:41 +0100

    factory usage

--- croneTabPage.py
+++ croneTabPage.py
@@ -1,9 +1,14 @@
 import webapp2
+import dataProviderFactory
 
 class CroneTabPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, chornetab!')
+
+	myFactory = ChroneTab()
+	myData = myFactory.createDataProvider()
+	buffer = myData.RetrieveSourcePage()
 
 app = webapp2.WSGIApplication([
     ('/', CroneTabPage),
