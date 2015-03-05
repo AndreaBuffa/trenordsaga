3ff422096defd23b271201ccd55c68de59e84fae
Andrea Buffa <A.Buffa@ext.adbglobal.com>
2015-03-05 16:09:41 +0100

    datetime picker added

--- stats.py
+++ stats.py
@@ -1,8 +1,9 @@
 import webapp2
-from controller import DayController
+from controller import *
 
 app = webapp2.WSGIApplication([
 	(r'/survey/(\d{4})/(\d{1,2})/(\d{1,2})', DayController),
 	(r'/survey/', DayController),
-	(r'/survey', DayController)
+	(r'/survey', DayController),
+	(r'/', Controller)
 ])
