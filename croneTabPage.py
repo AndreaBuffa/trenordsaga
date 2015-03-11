29ddd94c704ed8d7cd33af5203f7bb34cde0606c
Andrea Buffa <A.Buffa@ext.adbglobal.com>
2015-03-11 12:18:16 +0100

    mean batch bug fix

--- croneTabPage.py
+++ croneTabPage.py
@@ -48,24 +48,28 @@ class Mean(webapp2.RequestHandler):
 		self.response.write('Computing Mean, working...')
 		#delete accumulators
 		trainstops = TrainStop.query(TrainStop.trainid == '24114')
-		ndb.delete_multi([key for key in trainstops.iter(keys_only=True)])
+		ndb.delete_multi([key for key in trainstops.iter(keys_only = True)])
 		#create new ones
 		timings = S9.query().fetch()
 		for record in timings:
 				myParser = ScheduleParser(record.timings)
 				timeSchedule = myParser.GetTimings()
 				for entry in timeSchedule:
+					query = TrainStop.query(ndb.AND(TrainStop.name == entry['name'],
+												TrainStop.trainid == '24114'))
+					station = query.get()
+					if not station:
 						station = TrainStop()
 						station.name = entry['name']
 						station.trainid = '24114'
 						station.startdate = datetime.datetime.strptime('2014-12-18', "%Y-%m-%d")
 						station.numofsurveys = 0
 						station.delay = 0
-						station.numofsurveys += 1
-						if entry['delay_m'] > 0:
-								station.delay += entry['delay_m']
-						station.certainty = True if entry['certainty'] else False
-						station.put()
+					station.numofsurveys += 1
+					if entry['delay_m'] > 0:
+							station.delay += entry['delay_m']
+					station.certainty = True if entry['certainty'] else False
+					station.put()
 		self.response.write('Done!')
 
 mean = webapp2.WSGIApplication([
