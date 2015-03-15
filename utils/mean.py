4368fbf1e04648039cbd69ae7c52a204a6d7beb7
AB <andreabuffasbru@gmail.com>
2015-03-16 00:47:11 +0100

    cursor iterator added for the task queue

--- utils/mean.py
+++ utils/mean.py
@@ -8,28 +8,28 @@ from model.trainStop import *
 from view.scheduleParser import *
 import common
 
-BATCH_SIZE = 30  # ideal batch size may vary based on entity size.
+BATCH_SIZE = 30  # ideal batch size
 
 def Mean(cursor=None, num_updated=0):
     stops = {}
-    query = S9.query()
+    queryTimeSchedules = S9.query()
     if cursor:
         query = TrainStop.query(TrainStop.trainid == '24114')
         results = query.fetch()
         for record in results:
             stops[record.name] = record
-        query.with_cursor(cursor)
+        timings, cursor, more = queryTimeSchedules.fetch_page(BATCH_SIZE, start_cursor=cursor)
     else:
         trainstops = TrainStop.query(TrainStop.trainid == '24114')
         ndb.delete_multi([key for key in trainstops.iter(keys_only = True)])
+        timings, cursor, more = queryTimeSchedules.fetch_page(BATCH_SIZE)
 
-    timings = query.fetch(limit=BATCH_SIZE)
     if len(timings):
         common.createTrainStop(timings, stops)
         for key, stop in stops.iteritems():
             stop.put()
         num_updated += len(timings)
-        deferred.defer(Mean, cursor=query.iter(), num_updated=num_updated)
+        deferred.defer(Mean, cursor=cursor, num_updated=num_updated)
     else:
         logging.debug('Mediana computation complete with %d updates!', num_updated)
 
